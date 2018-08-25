0c08a9ccc80ee3910228053a53be1d9e278b5135
Travis Neufeld <travis@Traviss-MacBook-Pro.local>
2018-08-25 10:25:01 -0700

    Implement text input

--- FoodInput.js
+++ FoodInput.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default class FoodInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: ''
+        };
+    }
+
     render() {
         return (
             <View>
-                <Text>Text Input goes here</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Whad jeat?"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Button
+                    style={styles.submitButton}
+                    title="Submit"
+                    onPress={this.onSubmit}
+                />
             </View>
         )
     }
+
+    onSubmit = () => {
+        const { navigation } = this.props;
+        const submit = navigation.getParam('submit', () => console.log('fail'));
+        submit(this.state.text);
+        this.props.navigation.navigate('Home');
+    }
 }
+
+const styles = StyleSheet.create({
+    textInput: {
+        height: 40,
+    },
+    submitButton: {
+        paddingTop: 15
+    }
+});
--- FoodList.js
+++ FoodList.js
@@ -3,16 +3,25 @@ import { StyleSheet, View, FlatList, Button } from 'react-native';
 
 export default class FoodList extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [{
+        key: 'Tacos'
+      }]
+    };
+  }
+
   render() {
     return (
       <View  style={styles.container}>
+        <Button
+            title="What are you eating?"
+            onPress={() => this.props.navigation.navigate('FoodInput', { submit: this.submit.bind(this) })}
+        />
         <FlatList
-          data={[
-            {key: 'Tacos'},
-            {key: 'Steak'},
-            {key: 'Beer'},
-            {key: 'Salad'},
-          ]}
+          data={this.state.items}
           renderItem={this.renderItem}
         />
       </View>
@@ -22,15 +31,28 @@ export default class FoodList extends React.Component {
   renderItem = ({item}) => {
     return (
         <Button
+            style={styles.listItem}
             title={item.key}
-            onPress={() => this.props.navigation.navigate('FoodInput')}
+            onPress={() => this.props.navigation.navigate('FoodInput', { submit: this.submit.bind(this) })}
         />
     )
+  }
+
+  submit(newItem) {
+    const { items } = this.state;
+
+    items.push({ key: newItem });
+
+    this.setState({items: items.slice()});
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start'
+  },
+  listItem: {
+    color: 'black'
   }
 });
